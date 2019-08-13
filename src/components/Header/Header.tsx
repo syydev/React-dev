dcfdfc8f2dddf4cb99d1cf899e156bc8b6cf1ae5
SeungyoungYang <2014112096@dongguk.edu>
2019-08-13 23:07:48 +0900

    Modify app header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AuthCheck } from '../../utils/Auth';
+import { useDispatch } from 'react-redux';
+import './style.scss';
+
+const Header: React.FC = () => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className='app-header'>
+      <Link className='app-title' to='./'>React-dev</Link>
+      {!AuthCheck() ? (
+        <nav className="header-nav">
+          <Link className="header-nav-item" to='./login'>Login</Link>
+        </nav>
+      ) : (
+          <nav className="header-nav">
+            <Link className='header-nav-item' to='./'>{localStorage.getItem('id')}</Link>
+            <a className='header-nav-item' onClick={() => dispatch({ type: 'LOGOUT' })}>Logout</a>
+          </nav>
+        )}
+    </div>
+  );
+}
+
+export default Header;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,5 @@
-import React from 'react';
-import Login from './Login'
+import Header from './Header';
 
-const Header: React.FC = () => {
-  return (
-    <React.Fragment>
-      <Login/>
-    </React.Fragment>
-  );
-}
-
-export default Header;
+export {
+  Header
+};
