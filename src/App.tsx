98690f5a80550bcdb1bde6c7defb453e23cac7a2
SeungyoungYang <2014112096@dongguk.edu>
2019-08-24 19:49:41 +0900

    Add simple app scss

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,19 @@ import HeaderContainer from './containers/HeaderContainer';
 import HomeContainer from './containers/HomeContainer'
 import { Login } from './components/Auth';
 import { PostList } from './components/PostList'
+import './style.scss'
 
 const App: React.FC = () => {
   return (
     <React.Fragment>
       <HeaderContainer />
       <Switch>
-        <Route exact path="/" component={HomeContainer} />
-        <Route exact path="/counter" component={CounterContainer} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/post" component={PostList} />
+        <div className='content'>
+          <Route exact path="/" component={HomeContainer} />
+          <Route exact path="/counter" component={CounterContainer} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/post" component={PostList} />
+        </div>
       </Switch>
     </React.Fragment>
   );
