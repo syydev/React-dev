9b5f3c36bf818127c78efd7f6707a7cf86ea6a78
SeungyoungYang <2014112096@dongguk.edu>
2019-09-22 00:01:21 +0900

    Apply styled-component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { UserAction } from '../../store/user/user.action'
 import { IState } from '../../type';
-import './style.scss';
+import styled from 'styled-components';
 
 const Header: React.FC = () => {
   const dispatch = useDispatch();
@@ -13,22 +13,78 @@ const Header: React.FC = () => {
   }));
 
   return (
-    <div className='app-header'>
-      <Link className='app-title' style={{ textDecoration: 'none' }} to='/'>React-dev</Link>
+    <React.Fragment>
+    <AppHeader>
+      <AppTitle to='/'>React-dev</AppTitle>
       {!isLoggedIn ? (
-        <nav className="header-nav">
-          <NavLink className="header-nav-item" activeClassName="active" style={{ textDecoration: 'none' }} to='/login'>Login</NavLink>
-          <NavLink className="header-nav-item" activeClassName="active" style={{ textDecoration: 'none' }} to='/signup'>SignUp</NavLink>
-        </nav>
+        <AppNav>
+          <AppNavLink activeClassName="active" to='/login'>Login</AppNavLink>
+          <AppNavLink activeClassName="active" to='/signup'>SignUp</AppNavLink>
+        </AppNav>
       ) : (
-          <nav className="header-nav">
-            <NavLink className='header-nav-item' activeClassName="active" style={{ textDecoration: 'none' }} to='/post'>Post List</NavLink>
-            <NavLink className='header-nav-item' activeClassName="active" style={{ textDecoration: 'none' }} to='/user'>{name}</NavLink>
-            <a className='header-nav-item' onClick={() => dispatch(UserAction.logout.index())}>Logout</a>
-          </nav>
+          <AppNav>
+            <AppNavLink activeClassName="active" to='/post'>Post List</AppNavLink>
+            <AppNavLink activeClassName="active" to='/user'>{name}</AppNavLink>
+            <AppNavItem className='header-nav-item' onClick={() => dispatch(UserAction.logout.index())}>Logout</AppNavItem>
+          </AppNav>
         )}
-    </div>
+    </AppHeader>
+    <Dummy />
+    </React.Fragment>
   );
 };
+
+const AppHeader = styled.div`
+  position:fixed;
+  background: white;
+  padding: 15px;
+  width: 100%;
+  border-bottom: 1px solid #C7CED5;
+  z-index: 9999;
+`;
+
+const AppTitle = styled(Link)`
+  display: inline-block;
+  text-decoration: none;
+  font-size: 25px;
+  font-weight: 700;
+  color: black;
+`;
+
+const AppNav = styled.nav`
+  /* display: inline-block; */
+  float: right;
+  font-weight: 700;
+  right: 20px;
+`;
+
+const AppNavLink = styled(NavLink)`
+  text-decoration: none;
+  font-size: 17px;
+  color: #A6A6A6;
+  margin-right: 15px;
+
+  &:hover {
+    color: black;
+  }
+  &.active {
+    color: black;
+  }
+`;
+
+const AppNavItem = styled.a`
+  font-size: 17px;
+  color: #A6A6A6;
+  margin-right: 15px;
+  cursor: pointer;
+
+  &:hover {
+    color: black;
+  }
+`;
+
+const Dummy = styled.div`
+  padding: 32px;
+`;
 
 export default Header;
